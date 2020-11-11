c80d5eaea1aff3fc99fb82b2d9e7bf5ba2ef4150
Winni Neessen <wn@neessen.net>
2020-11-11 10:03:10 +0100

    v2.0.4 part 2

--- dist/lib/websiteBenchConfig.js
+++ dist/lib/websiteBenchConfig.js
@@ -5,7 +5,7 @@ const process_1 = require("process");
 class WebsiteBenchConfig {
     constructor(confFiles, logObj) {
         this._configObj = {};
-        this._versionNum = '2.0.3b';
+        this._versionNum = '2.0.4';
         this._allowCaching = false;
         this._ignoreSslErrors = false;
         this._logResourceErrors = false;
